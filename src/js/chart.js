7e2727455b8a77a3ae1e4d0c9006a3a6e8952254
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-02-26 22:01:00 +0100

    Display most appliced course data in bar chart

--- src/js/chart.js
+++ src/js/chart.js
@@ -12,41 +12,43 @@ async function getData() {
   
       const json = await response.json();
       console.log(json);
+      return json;
     } catch (error) {
       console.error(error.message);
+      return [];
     }
   }
 
 getData();
 
-/* 
 (async function() {
-    const data = [
-      { year: 2010, count: 10 },
-      { year: 2011, count: 20 },
-      { year: 2012, count: 15 },
-      { year: 2013, count: 25 },
-      { year: 2014, count: 22 },
-      { year: 2015, count: 30 },
-      { year: 2016, count: 28 },
-    ];
-  
+    const data = await getData()
+
+    // filter out courses
+    const courses = data.filter(item => item.type === "Kurs");
+
+    // sort by total number of applicants (high to low)
+    courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+    // get the top 6 courses
+    const topCourses = courses.slice(0, 6);
+
     new Chart(
-      document.getElementById('acquisitions'),
+      document.getElementById('barChart'),
       {
         type: 'bar',
         data: {
-          labels: data.map(row => row.year),
+          labels: topCourses.map(course => course.name),
           datasets: [
             {
-              label: 'Acquisitions by year',
-              data: data.map(row => row.count)
+              label: 'Mest Sökta Kurserna',
+              data: topCourses.map(course => course.applicantsTotal),
+              backgroundColor: 'rgba(75, 192, 192, 0.5)'
             }
           ]
         }
       }
     );
   })();
-*/
 
 /* pie chart */
