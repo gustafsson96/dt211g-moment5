8d1ff9b9712cd8366e8e1d47904406e50ebcba0a
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-02-27 20:41:39 +0100

    Test geolocation

--- src/js/map.js
+++ src/js/map.js
@@ -1,1 +1,16 @@
 "use strict";
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+        console.log("Latitude: " + latitude);
+        console.log("Longitude: " + longitude);
+    }, function (error) {
+        console.error("Fel vid inhämtning av position :", error.message)
+    });
+} else {
+    console.error("Geolokalisering stöds inte av din webbläsare");
+}
