5cd7c362c698ac86d72676b3238d267a43111e54
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-02-27 11:06:13 +0100

    Update comments and move pie chart labels to the right

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,82 +1,95 @@
 import Chart from 'chart.js/auto';
 
-/* bar chart */
 
+/* collect course and program data from external url */
 async function getData() {
-    const url = "https://studenter.miun.se/~mallar/dt211g/";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      return json;
-    } catch (error) {
-      console.error(error.message);
-      return [];
+  const url = "https://studenter.miun.se/~mallar/dt211g/";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error.message);
+    return [];
   }
+}
 
 getData();
 
-(async function() {
-    const data = await getData()
+/* create bar chart for 6 most popular courses */
+(async function () {
+  const data = await getData()
 
-    // filter out courses
-    const courses = data.filter(item => item.type === "Kurs");
+  // filter out courses
+  const courses = data.filter(item => item.type === "Kurs");
 
-    // sort by total number of applicants (high to low)
-    courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+  // sort by total number of applicants (high to low)
+  courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
 
-    // get the top 6 courses
-    const topCourses = courses.slice(0, 6);
+  // get the top 6 courses
+  const topCourses = courses.slice(0, 6);
 
-    new Chart(
-      document.getElementById('barChart'),
-      {
-        type: 'bar',
-        data: {
-          labels: topCourses.map(course => course.name),
-          datasets: [
-            {
-              label: 'Mest Sökta Kurserna',
-              data: topCourses.map(course => course.applicantsTotal),
-              backgroundColor: 'rgba(75, 192, 192, 0.5)'
+  new Chart(
+    document.getElementById('barChart'),
+    {
+      type: 'bar',
+      data: {
+        labels: topCourses.map(course => course.name),
+        datasets: [
+          {
+            label: 'Antal sökande',
+            data: topCourses.map(course => course.applicantsTotal),
+            backgroundColor: 'rgba(75, 192, 192, 0.5)'
+          }
+        ]
+      }
+    }
+  );
+})();
+
+/* create pie chart for 5 most popular programs */
+(async function () {
+  const data = await getData();
+
+  // filter out programs
+  const programs = data.filter(item => item.type === "Program");
+
+  // sort by total number of applicants (high to low)
+  programs.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+  // get the top 5 programs
+  const topPrograms = programs.slice(0, 5);
+
+  new Chart(
+    document.getElementById('pieChart'),
+    {
+      type: 'pie',
+      data: {
+        labels: topPrograms.map(program => program.name),
+        datasets: [
+          {
+            label: 'Antal sökande',
+            data: topPrograms.map(program => program.applicantsTotal),
+            backgroundColor: ["red", "yellow", "blue", "green", "pink"]
+          }
+        ]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: 'right',
+            align: 'center',
+            labels: {
+              boxWidth: 20,
+              padding: 15 
             }
-          ]
+          }
         }
       }
-    );
-  })();
-
-/* pie chart */
-(async function() {
-    const data = await getData()
-
-    // filter out programs
-    const programs = data.filter(item => item.type === "Program");
-
-    // sort by total number of applicants (high to low)
-    programs.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
-
-    // get the top 5 programs
-    const topPrograms = programs.slice(0, 5);
-
-    new Chart(
-      document.getElementById('pieChart'),
-      {
-        type: 'pie',
-        data: {
-          labels: topPrograms.map(program => program.name),
-          datasets: [
-            {
-              label: 'Mest Sökta Kurserna',
-              data: topPrograms.map(program => program.applicantsTotal),
-              backgroundColor: 'rgba(75, 192, 192, 0.5)'
-            }
-          ]
-        }
-      }
-    );
-  })();
+    }
+  );
+})();
