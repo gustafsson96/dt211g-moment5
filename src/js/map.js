83dc1c6cade6412681c881166d957c38f1f81241
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-02-28 10:44:29 +0100

    Add if statement to search form function to prevent console errors

--- src/js/map.js
+++ src/js/map.js
@@ -6,53 +6,58 @@
  * 
  * @param {Event} The submit event from map search bar
  */
-document.getElementById("search-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
 
-    const userInput = event.target.querySelector('input').value.trim();
+const searchForm = document.getElementById("search-form");
 
-    const emptyFieldMsg = document.querySelector('.empty-field');
+if (searchForm) {
+    searchForm.addEventListener("submit", async function (event) {
+        event.preventDefault();
 
-    if (!userInput) {
-        emptyFieldMsg.textContent = "Vänligen ange en plats";
-        emptyFieldMsg.classList.add('show');
+        const userInput = event.target.querySelector('input').value.trim();
 
-        setTimeout(() => {
-            emptyFieldMsg.classList.remove('show'); 
-        }, 2000);
+        const emptyFieldMsg = document.querySelector('.empty-field');
 
-        return;
-    }
+        if (!userInput) {
+            emptyFieldMsg.textContent = "Vänligen ange en plats";
+            emptyFieldMsg.classList.add('show');
 
-    const encodedInput = encodeURIComponent(userInput);
-    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodedInput}`;
+            setTimeout(() => {
+                emptyFieldMsg.classList.remove('show');
+            }, 2000);
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        if (data.length === 0) {
-            alert(`Inga resultat för ${userInput}. Vänligen testa igen.`);
             return;
         }
 
-        console.log("Sökresultat:", data);
+        const encodedInput = encodeURIComponent(userInput);
+        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodedInput}`;
 
-        const { lat, lon, display_name } = data[0];
-        const locationInfo = document.getElementById("location-info");
-        locationInfo.innerHTML = `<i>Plats: ${display_name}</i> <br>
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            if (data.length === 0) {
+                alert(`Inga resultat för ${userInput}. Vänligen testa igen.`);
+                return;
+            }
+
+            console.log("Sökresultat:", data);
+
+            const { lat, lon, display_name } = data[0];
+            const locationInfo = document.getElementById("location-info");
+            locationInfo.innerHTML = `<i>Plats: ${display_name}</i> <br>
                                   <i>Lat: ${lat}</i> <br>
                                   <i>Lon: ${lon}</i>`;
 
-        // update src attribute to move marker
-        const mapFrame = document.getElementById("map-frame");
-        mapFrame.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon}%2C${lat}%2C${lon}%2C${lat}&layer=mapnik&marker=${lat}%2C${lon}`;
+            // update src attribute to move marker
+            const mapFrame = document.getElementById("map-frame");
+            mapFrame.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon}%2C${lat}%2C${lon}%2C${lat}&layer=mapnik&marker=${lat}%2C${lon}`;
 
-    } catch (error) {
-        console.error("Error när plats skulle hämtas: ", error);
-        alert("Något gick fel. Vänligen testa igen.");
-    }
-});
+        } catch (error) {
+            console.error("Error när plats skulle hämtas: ", error);
+            alert("Något gick fel. Vänligen testa igen.");
+        }
+    });
+}
